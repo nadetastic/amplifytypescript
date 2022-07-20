ce4911ce1b5a971ca4acbdfa4adbf8e198557b23
dkkiuna <dkkiuna@amazon.com>
2022-07-19 21:46:19 -0500

    testing in dwbi

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "amplifytypescript": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+// import React from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { API } from "aws-amplify";
+import { listTodos } from './graphql/queries';
+import { ListTodosQuery } from './API';
 
-function App() {
+function App(): JSX.Element {
+
+  const [todos,set_todos] = useState<ListTodosQuery| null>(null);
+
+  useEffect(()=> {
+    fetchTodos();
+  },[]);
+
+  const fetchTodos = async () => {
+    try {
+      const resp = (await API.graphql({ query: listTodos})) as { data: ListTodosQuery} 
+      set_todos(resp.data);
+    } catch (e) { console.log(e) }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Todos (TypeScript React)</h1>
+      {todos?.listTodos?.items.map((t,i) => (
+        <div key={i}>{t.name}</div>
+      ))}
     </div>
   );
 }
